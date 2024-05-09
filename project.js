b066733d375c32600ae5eac3fdb9719501301c08
Akhila-tk <69979070+Akhila-tk@users.noreply.github.com>
2024-05-09 18:45:07 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,34 @@
+let  cityName=document.getElementById('cityName');
+let cityDetails=document.getElementById('cityDetails');
+let temperature=document.getElementById('temperature');
+
+
+let fetchDetails= async()=>{
+    // alert("its working")
+    if(cityName.value.length==0){
+        alert("please enter a city name before searching")
+    }
+    else{
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`
+    let response=await fetch(url)
+    let data=await response.json()
+    // console.log(data);
+    cityDetails.innerText=`${data.name},${data.sys.country}`
+    let x=new Date()
+        date.innerText=`📅${x.toDateString()}`
+        // temperature.innerText=data.main.temp
+        temperature.innerHTML=`${data.main.temp}<sup>o</sup>C`
+        if(data.main.temp>30){
+            temperature.innerHTML=`${data.main.temp}<sup>o</sup>C 🔥`
+        }
+        if(data.main.temp>=20 && data.main.temp<30){
+            temperature.innerHTML=`${data.main.temp}<sup>o</sup>c 🌞`
+        }
+        if(data.main.temp<20 && data.main.temp>0){
+            temperature.innerHTML=`${data.main.temp}<sup>o</sup>c ❄`
+        }
+        if(data.main.temp<0){
+            temperature.innerHTML=`${data.main.temp}<sup>o</sup>c ⛄`
+        }
+    }
+}
